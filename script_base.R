6c0212b7c4e909e4cf09e322d019d8d3d92e7e54
fabiorcampos <fabiorochacampos@gmail.com>
2017-08-08 13:18:58 -0300

    upload

--- script_base.R
+++ script_base.R
@@ -1,7 +1,10 @@
+library(ggplot2)
+library(datasets)
+
 ### summary the data
 summary(mtcars)
 plot(mtcars)
-cor(mtcars)
+cor(mtcars[, -c(9)])[1, ]
 
 ### Transpose to a factor
 mtcars$am = as.factor(mtcars$am)
@@ -15,20 +18,12 @@ autmanbox = boxplot(mpg ~ am, data=mtcars, main = "Comparison of MPG by type of 
               col = c("blue", "red"))
 
 ### Scatter Plot 
-par(mfrow=c(2, 2))
 wtplot = scatterplot(mpg ~ wt | am, data=mtcars,
                      xlab="Weight of Car", ylab="Miles Per Gallon",
                      main="MPG by Type of Gear and Weight",
                      col = c("blue", "red"),
                      legend.title = "Type of Transmission",
                      legend.coords = "topright") 
-
-hpplot = scatterplot(mpg ~ hp | am, data=mtcars,
-                     xlab="Horse Power", ylab="Miles Per Gallon",
-                     main="MPG by Type of Gear and Horse Power",
-                     legend.title = "Type of Transmission",
-                     legend.coords = "topright",
-                     col = c("blue", "red"))
 
 ### Models
 dot = mtcars[order(mtcars$mpg),] # sort by mpg
@@ -41,10 +36,10 @@ dotchart(dot$mpg,labels=row.names(dot),cex=.7,groups= dot$am,
 
 ### Regression Model
 fit1 = lm(mpg ~ wt, data=mtcars)
-fit2 = lm(mpg ~ hp, data=mtcars)
-fit3 = lm(mpg ~ am, data=mtcars)
+fit2 = lm(mpg ~ wt + am, data=mtcars)
+fit3 = lm(mpg ~ am + wt, data=mtcars)
 
 par(mfcol = c(1, 3))
 plot(mtcars$wt, resid(fit1), main = "Model 1", xlab = "Weight (lbs/1000)", ylab = "Residuals")
 plot(mtcars$wt, resid(fit2), main = "Model 2", xlab = "Weight (lbs/1000)", ylab = "Residuals")
-plot(mtcars$hp, resid(fit3), main = "Model 3", xlab = "Horse Power", ylab = "Residuals")
+plot(mtcars$wt, resid(fit3), main = "Model 3", xlab = "Weight (lbs/1000)", ylab = "Residuals")
